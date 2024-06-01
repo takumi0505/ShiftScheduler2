de712efbb9da974d0ad85b4d0130d8d1b2333a7e
takumi0505 <takumi.teshima0529@gmail.com>
2024-06-01 12:06:08 +0900

    Initial commit

---
+++ src/test/java/com/example/ShiftScheduler/ShiftSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShiftScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
